a10dd1b3533baf56dbcc2ab10fe59ecb3efb910f
isaac.wadd <isaac.wadd@gmail.com>
2024-07-19 17:04:35 -0600

    configuring for deployment

--- dummydatabases/dummydatabases/settings.py
+++ dummydatabases/dummydatabases/settings.py
@@ -149,6 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dummydatabases/static'),
+    '/var/www/html/static',
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
