f3fa76317ffd6dbc5824081834a3fcf2121c873a
isaac.wadd <isaac.wadd@gmail.com>
2024-07-19 16:13:41 -0600

    configuring for deployment

--- dummydatabases/dummydatabases/settings.py
+++ dummydatabases/dummydatabases/settings.py
@@ -152,5 +152,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_REDIRECT_URL = '/'
