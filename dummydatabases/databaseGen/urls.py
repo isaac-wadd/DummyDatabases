eaa8035fe2723ac5328a315f7f82055ddda9e772
isaac.wadd <isaac.wadd@gmail.com>
2024-07-20 11:31:38 -0600

    configuring for deployment

--- dummydatabases/databaseGen/urls.py
+++ dummydatabases/databaseGen/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from .views import *
 from django.urls import include, path, re_path
 from django.views.static import serve
+from django.conf import settings
 
 static_urlpatterns = [
-    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns = [
