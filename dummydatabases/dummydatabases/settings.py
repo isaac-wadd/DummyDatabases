a25e48d5af13840963b95e6a46a5ce7416dcce18
isaac.wadd <isaac.wadd@gmail.com>
2024-07-20 11:44:32 -0600

    configuring for deployment

--- dummydatabases/dummydatabases/settings.py
+++ dummydatabases/dummydatabases/settings.py
@@ -147,14 +147,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = '/var/www/html/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/html/static/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dummydatabases/static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/html/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = [
